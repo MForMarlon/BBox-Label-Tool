7abe04f8c235fea45462a8fdba0f176eaff7c91e
Marlon E <marlon_evangelista@gensler.com>
2019-02-27 14:25:25 -0800

    Ensure main.py works in Python 3.x

--- main.py
+++ main.py
@@ -3,12 +3,10 @@
 # Purpose:     Label object bboxes for ImageNet Detection data
 # Author:      Qiushi
 # Created:     06/06/2014
-
-#
 #-------------------------------------------------------------------------------
 from __future__ import division
-from Tkinter import *
-import tkMessageBox
+from tkinter import *
+import tkinter.messagebox
 from PIL import Image, ImageTk
 import os
 import glob
@@ -17,7 +15,7 @@ import random
 # colors for the bboxes
 COLORS = ['red', 'blue', 'yellow', 'pink', 'cyan', 'green', 'black']
 # image sizes for the examples
-SIZE = 256, 256
+SIZE = 720, 480
 
 class LabelTool():
     def __init__(self, master):
@@ -133,7 +131,7 @@ class LabelTool():
         self.imageDir = os.path.join(r'./Images', '%03d' %(self.category))
         self.imageList = glob.glob(os.path.join(self.imageDir, '*.JPEG'))
         if len(self.imageList) == 0:
-            print 'No .JPEG images found in the specified dir!'
+            print ('No .JPEG images found in the specified dir!')
             return
 
         # default to the 1st image in the collection
@@ -164,7 +162,7 @@ class LabelTool():
             self.egLabels[i].config(image = self.egList[-1], width = SIZE[0], height = SIZE[1])
 
         self.loadImage()
-        print '%d images loaded from %s' %(self.total, s)
+        print ('%d images loaded from %s' %(self.total, s))
 
     def loadImage(self):
         # load image
@@ -203,8 +201,7 @@ class LabelTool():
             f.write('%d\n' %len(self.bboxList))
             for bbox in self.bboxList:
                 f.write(' '.join(map(str, bbox)) + '\n')
-        print 'Image No. %d saved' %(self.cur)
-
+        print ('Image No. %d saved' %(self.cur))
 
     def mouseClick(self, event):
         if self.STATE['click'] == 0:
